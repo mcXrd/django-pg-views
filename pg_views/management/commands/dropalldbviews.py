29aef1d4cfe2f2982ccab981384ae62ac80cc8ef
Lubos Matl <matllubos@gmail.com>
2015-02-05 19:18:24 +0100

    v0.0.2 Add drop all views command

---
+++ pg_views/management/commands/dropalldbviews.py
@@ -0,0 +1,19 @@
+from django.core.management.base import NoArgsCommand
+from django.db import connection
+
+
+class Command(NoArgsCommand):
+
+    help = 'Drop all DB views.'
+
+    def handle_noargs(self, **options):
+        cursor = connection.cursor()
+        cursor.execute('''SELECT 'DROP VIEW "' || table_name || '";'
+                          FROM information_schema.views
+                          WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+                          AND table_name !~ '^pg_';''')
+        for drop_command in cursor.fetchall():
+            self.stdout.write('Drop view %s' % drop_command[0].split('"')[1])
+            cursor.execute(drop_command[0])
+        else:
+            self.stdout.write('No view was dropped')

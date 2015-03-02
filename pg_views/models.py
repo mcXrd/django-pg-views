019b9796c0faa20025329b8df800e6370a6419e1
Lubos Matl <matllubos@gmail.com>
2015-03-02 22:30:44 +0100

    v0.0.3 add column names mapping

--- pg_views/models.py
+++ pg_views/models.py
@@ -7,6 +7,7 @@ from .loading import register_sql_model_view
 
 
 class ModelViewBase(type):
+
     def __new__(cls, *args, **kwargs):
         name, _, attrs = args
         abstract = attrs.pop('abstract', False)
@@ -25,15 +26,20 @@ class ModelView(six.with_metaclass(ModelViewBase)):
     view_name = None
     upper_names = True
     exclude = ()
+    column_name_mapping = {}
+
+    def get_column_name(self, model_column_name):
+        column_name = self.column_name_mapping.get(model_column_name, model_column_name)
+        return self.upper_names and column_name.upper() or column_name
 
     def get_columns(self):
         result = SortedDict()
         for field in self.model._meta.fields:
             attname, column = field.get_attname_column()
             if attname not in self.exclude:
-                result[self.upper_names and column.upper() or column] = column
+                result[self.get_column_name(column)] = column
         return result
 
     def get_name(self):
-        name = '%s_view' % self.model._meta.db_table
-        return self.view_name or (self.upper_names and name.upper() or name)
+        name = self.view_name or '%s_view' % self.model._meta.db_table
+        return self.upper_names and name.upper() or name
